233dd83b7cef9b4bad2b840575414d7e1b288390
Felipe Fransa <antoniofelipesilva@live.com>
2024-05-06 19:20:24 -0300

    method scanner

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,8 +1,14 @@
 package br.com.felipe.main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner reading = new Scanner(System.in);
+        System.out.println("Digite seu filme:");
+        String movie = reading.nextLine();
         System.out.println("Olá");
     }
 }
