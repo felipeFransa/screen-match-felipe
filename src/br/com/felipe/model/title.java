6b3c319e37f79d676e389ab63222d972a58ce968
Felipe Fransa <antoniofelipesilva@live.com>
2024-05-22 17:32:04 -0300

    add title

--- src/br/com/felipe/model/title.java
+++ src/br/com/felipe/model/title.java
@@ -1,4 +1,23 @@
 package br.com.felipe.model;
 
 public class title {
+
+    private String titleName;
+    private double titleAge;
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public void setTitleName(String titleName) {
+        this.titleName = titleName;
+    }
+
+    public double getTitleAge() {
+        return titleAge;
+    }
+
+    public void setTitleAge(double titleAge) {
+        this.titleAge = titleAge;
+    }
 }
