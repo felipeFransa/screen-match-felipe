d0cc18f956e4d2af9e121fa512f830ed6c6520f8
Felipe Fransa <antoniofelipesilva@live.com>
2024-05-23 18:24:01 -0300

    add season

--- src/br/com/felipe/model/serie.java
+++ src/br/com/felipe/model/serie.java
@@ -1,4 +1,14 @@
 package br.com.felipe.model;
 
 public class serie {
+
+    public double getTitleSeason() {
+        return titleSeason;
+    }
+
+    public void setTitleSeason(double titleSeanso) {
+        this.titleSeason = titleSeanso;
+    }
+
+    private double titleSeason;
 }
