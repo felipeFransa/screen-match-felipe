17fe50761ed7ba286e9a1f9c0c4ead881afc71b7
Felipe Fransa <antoniofelipesilva@live.com>
2024-05-24 19:13:06 -0300

    add movie method

--- src/br/com/felipe/model/movie.java
+++ src/br/com/felipe/model/movie.java
@@ -1,4 +1,13 @@
 package br.com.felipe.model;
 
 public class movie {
+    private double titleduration;
+
+    public double getTitleduration() {
+        return titleduration;
+    }
+
+    public void setTitleduration(double titleduration) {
+        this.titleduration = titleduration;
+    }
 }
