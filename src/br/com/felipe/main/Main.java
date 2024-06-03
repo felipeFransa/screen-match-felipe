8a64fb068ef0fbb9021b67f13e637bbf8c73d7a6
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-02 21:34:00 -0300

    new movie title

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -18,8 +18,16 @@ public class Main {
         title.setTitleAge(2002);
         title.setTitleNote(9.0);
 
+        Title newTitle = new Title();
+        newTitle.setTitleName("Avater");
+        newTitle.setTitleAge(2022);
+        newTitle.setTitleNote(8.5);
+
         System.out.println(title.getTitleName());
         System.out.println(title.getTitleAge());
         System.out.println(title.getTitleNote());
+
+        System.out.println("Meu filme: " +newTitle.getTitleName());
+        System.out.println("Ano de lancamento: " +newTitle.getTitleAge());
     }
 }
