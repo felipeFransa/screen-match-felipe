bddd0542fc2cf0904ff6282ff5a690fd01eca08d
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-19 18:13:19 -0300

    link api mdb

--- src/br/com/felipe/model/ApiMovie.java
+++ src/br/com/felipe/model/ApiMovie.java
@@ -1,5 +1,5 @@
 package br.com.felipe.model;
 
 public class ApiMovie {
-    String http = "http://www.omdbapi.com/?apikey=[yourkey]&";
+    String http = "https://www.omdbapi.com/?i=tt3896198&apikey=3f9c31bf";
 }
