388f9b421875ef2e906130d652b8b494ca0af84c
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-24 17:18:47 -0300

    if acesso

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,8 +1,19 @@
 package br.com.felipe.main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello welcome!");
+        Scanner reading = new Scanner(System.in);
+        System.out.println("Digite 1 ou 0;");
+        Integer opcion = reading.nextInt();
+
+        if (opcion > 0) {
+            System.out.println("Seja bem vindo:");
+        } else {
+            System.out.println("Acesso negado!");
+        }
     }
 }
