2be99c92eeb5228d2a28006fbcccb69d571075ff
Felipe Fransa <antoniofelipesilva@live.com>
2024-09-22 20:59:01 -0300

    class ADS

---
+++ src/br/com/felipe/screenMatch/model/ADS.java
@@ -0,0 +1,22 @@
+package br.com.felipe.screenMatch.model;
+
+public class ADS {
+    private String titleADS;
+    private String descriptionADS;
+
+    public String getTitleADS() {
+        return titleADS;
+    }
+
+    public void setTitleADS(String titleADS) {
+        this.titleADS = titleADS;
+    }
+
+    public String getDescriptionADS() {
+        return descriptionADS;
+    }
+
+    public void setDescriptionADS(String descriptionADS) {
+        this.descriptionADS = descriptionADS;
+    }
+}
