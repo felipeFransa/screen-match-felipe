d9aa1fb74859ab0a45000c6861db0e8c1b19a174
Felipe Fransa <antoniofelipesilva@live.com>
2024-10-27 21:14:29 -0300

    add new date main

--- src/br/com/felipe/screenMatch/main/Main.java
+++ src/br/com/felipe/screenMatch/main/Main.java
@@ -3,6 +3,8 @@ package br.com.felipe.screenMatch.main;
 import br.com.felipe.screenMatch.model.ApiMovie;
 import br.com.felipe.screenMatch.model.User;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +15,8 @@ public class Main {
         Scanner reading = new Scanner(System.in);
         User user = new User();
         ApiMovie api = new ApiMovie();
+        LocalTime myTime = LocalTime.now();
+        LocalDate myDate = LocalDate.now();
 
         System.out.println("Enter your name: ");
         String name = reading.nextLine();
@@ -24,6 +28,7 @@ public class Main {
         if ( verification.equals("Plus")) {
             System.out.println("Welcome ");
             System.out.println(user.getFirstName());
+            System.out.println(myDate + " " + myTime);
         } else {
             System.out.println("Access denied!");
         }
