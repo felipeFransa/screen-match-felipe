c90b79da13252287ebb86c01db0908b220bcf81c
Felipe Fransa <antoniofelipesilva@live.com>
2024-11-14 16:56:02 -0300

    add key api

--- src/br/com/felipe/screenMatch/model/ApiMovie.java
+++ src/br/com/felipe/screenMatch/model/ApiMovie.java
@@ -1,5 +1,5 @@
 package br.com.felipe.screenMatch.model;
 
 public class ApiMovie {
-    private String http = "http://img.omdbapi.com/?apikey=[yourkey]&\n";
+    private String http = "http://img.omdbapi.com/?apikey=[yourkey]&3f9c31bf\n";
 }
